09d404ad85e83ec8d5786a848d44b2783ab6d5e7
Pavel Shyrkavets <pshyrkavets@gmail.com>
2025-06-17 12:46:24 +0200

    Added a conditional skip and the @fast tag

--- e2e/playwright.spec.ts
+++ e2e/playwright.spec.ts
@@ -7,17 +7,18 @@ test.describe('playwright.dev tests', () => {
     await page.goto('https://playwright.dev/');
   });
 
-  test('playwright.dev URL', async ({ page }) => {
+  test('playwright.dev URL', {tag: '@fast'}, async ({ page, browserName }) => {
+    test.skip(browserName === 'firefox', 'firefox is intentionally skipped in this test');
     // Playwright assertions use the expect API
     await expect(page).toHaveURL('https://playwright.dev/');
   });
 
-  test('playwright.dev title', async ({ page }) => {
+  test('playwright.dev title', {tag: '@fast'}, async ({ page }) => {
     // playwright.dev title contains substring
     await expect(page).toHaveTitle(/Playwright/);
   });
 
-  test('Get started link', async ({ page }) => {
+  test('Get started link', {tag: '@fast'}, async ({ page }) => {
     await page.getByRole('link', { name: 'Get started' }).click();
 
     await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
